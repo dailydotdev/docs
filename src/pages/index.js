a4f81289711227e6fda65b95a683e507376d36e8
Saad Hasan <saadh393@gmail.com>
2022-08-04 10:13:47 +0600

    Backgraound Image fixed, card backdrop blur added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './video.module.css';
-import HomeNavBoxes from '../components/homepage/homeNavBoxes';
-import { Redirect } from '@docusaurus/router';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
+import React from "react";
+import HomeNavBoxes from "../components/homepage/homeNavBoxes";
+import styles from "./video.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    
     <header className={clsx(styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">daily.dev Documentation</h1>
@@ -24,11 +21,14 @@ export default function Home() {
 
   // return <Redirect to="/docs/intro" />;
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
+        <img
+          className={styles.heroBgImage}
+          src="https://assets.website-files.com/5e0a5d9d743608d0f3ea6753/619650c123dedf5abe6feaa5_CTA%20Footer%20Circles.png"
+          alt=""
+        />
         <HomeNavBoxes />
       </main>
     </Layout>
